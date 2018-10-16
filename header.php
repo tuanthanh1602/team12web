cd737e547a49d61774e91dc842afa4658e8ffd29
tuanthanh1602 <37056932+tuanthanh1602@users.noreply.github.com>
2018-10-16 12:48:51 +0700

    Update header.php

--- header.php
+++ header.php
@@ -13,15 +13,15 @@
         <li><a href="index.html"><b>Trang Chủ</b></a></li>
         <li><a href="index.html"><b>Menu2</b></a></li>
         <li><a href="index.html"><b>Menu3</b></a></li>
-
+        
       <li>
        <div id="info" style=" margin-left:1600px">
         <?php  if (isset($_SESSION['username'])) : ?>
           <p style="color:black">Xin Chào:  <strong><?php echo $_SESSION['username']; ?></strong></p>
           <p> <a href="index.php?logout='1'" style="color: rgb(210, 49, 20);"><strong>Đăng Xuất</strong></a> </p>
         <?php endif ?>
-      </div>
-    </li>
+        </div>
+      </li>
 
 
 
